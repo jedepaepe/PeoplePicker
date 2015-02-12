3c26e6fbb16c43be12088109be1ef360288b5b9d
jedepaepe <jedepaepe@epfc.eu>
2015-02-12 16:21:31 +0100

    Définition de différentes méthodes à implémenter.

--- src/loader/CvsLoader.java
+++ src/loader/CvsLoader.java
@@ -8,11 +8,49 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.io.File;
+import java.util.ArrayList;
 
 import model.Travailleur;
 
 public class CvsLoader {
-
+    
+    /**
+     * Load a cvs file, parse it and create a list of Travailleur
+     * @param fileName : path & name of the cvs file
+     * @return : the list of Travailleur
+     */
+    public static LinkedList<Travailleur> load(String fileName) {
+        LinkedList<Travailleur> lt = new LinkedList<>();
+        // todo code to load the list
+        return lt;
+    }
+    
+    /**
+     * Caution : test purpoes only
+     * Load an hard coded list of Travailleur.
+     * @return a list of Travailleur
+     */
+    public static LinkedList<Travailleur> loadTest() {
+        LinkedList<Travailleur> lt = new LinkedList<>();
+        // todo code to return a hard coded list
+        return lt;        
+    }
+    
+    /**
+     * Caution : test purpoes only
+     * load a list of Travailleur from a test file (file deployed by the program)
+     * @return a list of Travailleur
+     */
+    public static LinkedList<Travailleur> loadTestFile() {
+        LinkedList<Travailleur> lt = new LinkedList<>();
+        // todo : load the list from a test file (file controlled by the program)
+        return lt;
+    }
+    
+    /**
+     * use it only to test the class
+     * @param args 
+     */
     public static void main(String[] args) {
         try {
             File f = new File("Travailleur.csv");
