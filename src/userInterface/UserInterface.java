d7daaf2c1e35e8d54bfab47cea45d52ccb862213
2203allarreadelpozo <2203allarreadelpozo@PC8394.domepfc.net>
2015-02-12 21:26:04 +0100

    UI update

--- src/userInterface/UserInterface.java
+++ src/userInterface/UserInterface.java
@@ -5,8 +5,11 @@
  */
 package userinterface;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.Scanner;
+import loader.*;
+import model.*;
 
 /**
  *
@@ -19,6 +22,39 @@ public class UserInterface {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        LinkedList<model.Travailleur> listWorker= new LinkedList<model.Travailleur> () ;
+        
+        System.out.println(args.length);
+        if(args.length == 2) {
+            File f = new File(args[1]);
+            if(f.exists()) {
+                switch (args[0]) {
+                    case "L": {
+                        listWorker = loader.CvsLoader.load(args[1]);                        
+                        
+                        System.out.println("case L");
+                        break;
+                    }
+                    case "S": {
+                        System.out.println("case L");
+                        /*
+                        System.out.println(
+                                "Sur quoi voulez vous faire la recherche? (1: Prenom, 2:Nom, 3: Telephone");
+                        int reponse = sc.nextInt();
+                        userInterface.UISearch.searchTrav(reponse);
+                        */
+                    }
+                }
+            } else {
+                System.out.println("file doesn't exists");
+                System.exit(1);
+            }
+        } else {
+            System.out.println("invalid arguments");
+            System.exit(1);
+        }
+        
+        /*
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Voulez-vous charger un fichier (L) ou rechercher des données (S) ? ");
@@ -35,6 +71,6 @@ public class UserInterface {
                 userInterface.UISearch.searchTrav(reponse);
             }
         }
-
+        */
     }
 }
